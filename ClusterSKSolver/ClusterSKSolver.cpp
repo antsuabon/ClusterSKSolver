34162f66b01b587c9dbed39b4e967e6a05788f10
Antonio Suárez Bono <antsuabon@alum.us.es>
2021-03-21 12:51:25 +0100

    Proyecto vacío
    - Se ha creado un proyecto vacío con estructura CMake.
    
    Tarea relacionada: #1

---
+++ ClusterSKSolver/ClusterSKSolver.cpp
@@ -0,0 +1,12 @@
+﻿// ClusterSKSolver.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "ClusterSKSolver.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
